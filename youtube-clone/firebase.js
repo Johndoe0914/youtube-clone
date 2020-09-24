f518ce17225a1423a300f86929f71a5456c90d9c
Jonathan Diaz <jonathandiaz0914@gmail.com>
2020-09-24 17:33:01 -0400

    created navbar and sidebar components

---
+++ youtube-clone/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQI4JGWoryRSwUxeIzuPfI5k1abdXVTGM",
+  authDomain: "clone-8e6d3.firebaseapp.com",
+  databaseURL: "https://clone-8e6d3.firebaseio.com",
+  projectId: "clone-8e6d3",
+  storageBucket: "clone-8e6d3.appspot.com",
+  messagingSenderId: "1000225443568",
+  appId: "1:1000225443568:web:bf0928dfdabfb40ada7c35",
+  measurementId: "G-YWVT08WRRZ",
+};
---
+++ youtube-clone/src/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RecommendedVideos.css";
+
+const RecommendedVideos = () => {
+  return (
+    <div className='recommendedVideos'>
+      <h1>reocmmed vides</h1>
+    </div>
+  );
+};
+
+export default RecommendedVideos;
